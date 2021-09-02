1892b588fc9baacccbdd41687e412a2d5c79ef69
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-02 22:59:04 +0500

    Updates

--- src/errors/database-connection-error.ts
+++ src/errors/database-connection-error.ts
@@ -2,7 +2,7 @@ import { CustomError } from './custom-error'
 
 export class DatabaseConnectionError extends CustomError {
   statusCode = 500
-  reason = 'Error connecting to database. Check mongo connection.'
+  reason = 'Error connecting to database. Check connection.'
 
   constructor() {
     super('Error connecting to database')
