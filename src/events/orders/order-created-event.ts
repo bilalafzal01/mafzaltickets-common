56d49560ae583c893a1c949078d759964f44c4fa
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-04 01:52:27 +0500

    Updates

--- src/events/orders/order-created-event.ts
+++ src/events/orders/order-created-event.ts
@@ -7,7 +7,7 @@ export interface OrderCreatedEvent {
     id: string
     status: OrderStatus
     userId: string
-    expiredAt: string
+    expiresAt: string
     ticket: {
       id: string
       price: number
